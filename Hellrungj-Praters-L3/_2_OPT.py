d3d9f27b782474fa677fd61c878c6a6758a4c821
John Hellrung <hellrungj@berea.edu>
2016-01-28 03:01:32 +0000

    Renaming files for style
    praters-hellrungj-L3 -> Hellrungj-Praters-L3

---
+++ Hellrungj-Praters-L3/_2_OPT.py
@@ -0,0 +1,11 @@
+
+class _2_OPT():
+    def __init__(self):
+        pass
+
+    def stuff(self):
+        pass
+
+    def otherstuff(self):
+        pass
+
---
+++ Hellrungj-Praters-L3/test.py
@@ -0,0 +1,4 @@
+x='buffalo'
+exec("%s = %d" % (x,2))
+
+print buffalo
